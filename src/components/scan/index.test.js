01e2a7f720cae3d4d7bd3fcbaff98d4b6548529f
Anoop Santhanam <anoop.santhanam@gmail.com>
2023-12-20 05:40:07 +0530

    Added unit test for scan component

---
+++ src/components/scan/index.test.js
@@ -0,0 +1,74 @@
+// scan.test.js
+import React from "react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Scan } from ".";
+
+// Mock the fetch function
+global.fetch = jest.fn();
+
+describe("Scan", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders Scan component", () => {
+    render(<Scan setItem={() => {}} />);
+    expect(screen.getByTestId("scan")).toBeInTheDocument();
+  });
+
+  test("submits form and sets item on successful scan", async () => {
+    const setItemMock = jest.fn();
+    fetch.mockResolvedValueOnce({
+      json: () => ({
+        success: true,
+        data: {
+          name: "test",
+          sku: "123",
+        },
+      }),
+    });
+
+    render(<Scan setItem={setItemMock} />);
+
+    const skuInput = screen.getByTestId("sku");
+    fireEvent.change(skuInput, { target: { value: "123" } });
+
+    await act(async () => {
+      fireEvent.keyUp(skuInput, { keyCode: 13 });
+    });
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      "http://localhost:3000/scan?sku=123"
+    );
+
+    expect(setItemMock).toHaveBeenCalledWith({
+      name: "test",
+      sku: "123",
+    });
+  });
+
+  test("handles error on unsuccessful scan", async () => {
+    const setItemMock = jest.fn();
+    fetch.mockResolvedValueOnce({
+      json: () => ({ success: false, message: "Invalid item SKU" }),
+    });
+
+    render(<Scan setItem={setItemMock} />);
+
+    const skuInput = screen.getByTestId("sku");
+    fireEvent.change(skuInput, { target: { value: "456" } });
+
+    await act(async () => {
+      fireEvent.keyUp(skuInput, { keyCode: 13 });
+    });
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      "http://localhost:3000/scan?sku=456"
+    );
+
+    expect(setItemMock).toHaveBeenCalledWith(null);
+
+    expect(screen.getByText("Invalid item SKU")).toBeInTheDocument();
+  });
+});
